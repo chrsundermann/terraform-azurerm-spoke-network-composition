df0bd0a41875c0ffea6e498b58b34b9b73ec60d1
rigydi <rigydi@outlook.de>
2023-06-02 20:43:33 +0000

    ...

--- test/test.go
+++ test/test.go
@@ -14,10 +14,12 @@ func TestExample1(t *testing.T) {
 		TerraformDir: "./",
 		NoColor:      true,
 		EnvVars: map[string]string{
-			"ARM_CLIENT_ID":       os.Getenv("ARM_CLIENT_ID"),
-			"ARM_CLIENT_SECRET":   os.Getenv("ARM_CLIENT_SECRET"),
-			"ARM_TENANT_ID":       os.Getenv("ARM_TENANT_ID"),
-			"ARM_SUBSCRIPTION_ID": os.Getenv("ARM_SUBSCRIPTION_ID"),
+			"ARM_CLIENT_ID":              os.Getenv("ARM_CLIENT_ID"),
+			"ARM_CLIENT_SECRET":          os.Getenv("ARM_CLIENT_SECRET"),
+			"ARM_TENANT_ID":              os.Getenv("ARM_TENANT_ID"),
+			"ARM_SUBSCRIPTION_ID":        os.Getenv("ARM_SUBSCRIPTION_ID"),
+			"TF_VAR_subscription_id:"     os.Getenv("TF_VAR_subscription_id")
+			"TF_VAR_subscription_id_hub:" os.Getenv("TF_VAR_subscription_id_hub")
 		},
 	})
 
